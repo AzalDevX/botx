6aff664c901dfb02c0b4d96c0285a6912c0418b8
LaloruHub <gonzaloruizazaldegui@gmail.com>
2023-10-07 18:19:09 +0200

    Discord Notification 1.0

--- botx.py
+++ botx.py
@@ -10,11 +10,12 @@ intents.presences = False
 client = discord.Client(intents=intents)
 
 # Canal específico donde quieres detectar mensajes (reemplaza con el ID de tu canal)
-canal_id = 1153587296851988568 #PRUEBAS
-#canal_id = 982690448780963860   #TX DISCORD
+# canal_id = 1153587296851988568  # PRUEBAS
+canal_id = 982690448780963860  # TX DISCORD
 
 # Lista de mensajes enviados por el bot
 mensajes_enviados = []
+
 
 @client.event
 async def on_ready():
@@ -26,19 +27,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.channel.id == canal_id and not message.author.bot:
-        # El bot detecta un mensaje en el canal específico y el autor no es un bot
-        # Puedes agregar tu lógica aquí para responder al mensaje o realizar acciones específicas
-        await message.channel.send('@everyone ¡Nueva votación! Por favor, vota con cabeza. Gracias y un saludo.')
-        
-        # Agregar el mensaje a la lista de mensajes enviados
-        mensajes_enviados.append(message)
-        
-        # Programar la eliminación del mensaje después de un período de tiempo (por ejemplo, 10 segundos)
-        await asyncio.sleep(1)
-        
-        # Eliminar el mensaje de la lista después de que haya transcurrido el tiempo
-        mensajes_enviados.remove(message)
+    if message.channel.id == canal_id and message.author.id != client.user.id:
+        await sendDiscordMessage(message)
+
+
+async def deleteMessage(msg):
+    # Código para eliminar mensaje enviado
+    await msg.delete()
+
+
+async def sendDiscordMessage(msg):
+    if not msg.author.bot:
+        await deleteMessage(msg)
+        return
+    # El bot detecta un mensaje en el canal específico, el autor no es un bot y no es el propio bot
+    # Puedes agregar tu lógica aquí para responder al mensaje o realizar acciones específicas
+    await msg.channel.send('@everyone ¡Nueva votación! Por favor, vota con cabeza. Gracias y un saludo.')
+
+    # Agregar el mensaje a la lista de mensajes enviados
+    mensajes_enviados.append(msg)
+
+    # Programar la eliminación del mensaje después de un período de tiempo (por ejemplo, 10 segundos)
+    await asyncio.sleep(1)
+
+    # Eliminar el mensaje de la lista después de que haya transcurrido el tiempo
+    mensajes_enviados.remove(msg)
+
 
 # Inicia el bot con su token de acceso
-client.run('MTE1OTkxODQwNDUzMTEzODY1MQ.GTooPZ.Qor16zjyDVDNSlg_FNKhwAP4anmXxP14p0g3Tk')
+client.run(
+    'MTE1OTkxODQwNDUzMTEzODY1MQ.GTooPZ.Qor16zjyDVDNSlg_FNKhwAP4anmXxP14p0g3Tk')
